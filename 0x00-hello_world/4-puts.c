fe1acf597f360113520ae56d6a611efdf99ba12d
alameen738 <alameen738@gmail.com>
2023-06-17 09:29:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main . Hello put program
- * Return; 0 (Success)
+ * main . Hello, puts program
+ * Return; 0
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("/"Programming is like building a multilingual puzzle")
+	return (0);
 }
