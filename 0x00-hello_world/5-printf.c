55130c5af5cd5070cba3a7395cb1f5fde60e5309
alameen738 <alameen738@gmail.com>
2023-06-17 23:57:05 -0700

    5-printf.c5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main . A program to print Programming is like building a multilingual puzzle
+ * return 0 (Succeses)
+ */
+int main(void)
+{
+	print("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
