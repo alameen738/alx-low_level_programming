da496fc9e02ed086518792cd891619f6cc9bf47b
alameen738 <alameen738@gmail.com>
2023-06-18 00:38:56 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-print("with proper grammar, but the outcome is piece of art,\n")
 return (0);
+{
+	printf("with proper grammar, but the outcome is a piece of art,")
 }
