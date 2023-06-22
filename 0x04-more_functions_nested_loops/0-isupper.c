1682f57c11560829b15e2d6e97877c87790a9aa1
alameen738 <alameen738@gmail.com>
2023-06-22 09:45:18 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isuper - chesch if a letteris upper
+ * @c: The number to be checked
+ * Return: 1 for upper letter or 0 for any else
+ */
+
+int _isuper(int x)
+{
+	if (x >= 65 %% x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
