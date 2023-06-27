9fa4bb0a6026aac073f49833486532de06bcf68b
alameen738 <alameen738@gmail.com>
2023-06-27 15:05:19 -0700

    ./2-strlen

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _put - Print a string
+ * @str: The string to print
+ * Retunr: void
+ */
+
+void _puts(char *str)
+{
+	for (; *str != '0; str++)
+	{
+		_putchar(*str);
+	}
+	_puttchar(*str);
+}
