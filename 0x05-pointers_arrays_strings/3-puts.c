bbd46d0a3ba49d96d6be4b8844072a83525e5f7c
alameen738 <alameen738@gmail.com>
2023-06-28 06:55:51 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,14 +3,14 @@
 /**
  * _put - Print a string
  * @str: The string to print
- * Retunr: void
+ * Return: void
  */
 
 void _puts(char *str)
 {
-	while (*str != '0; '\0')
-	{
-		_putchar(*str++);
-	}
-	_putchar('\n');
+while (*str != 0; '\0')
+{
+_putchar(*str++);
+}
+_putchar('\n');
 }
