87c555ad232793a9015e0e36df17bb4a08f442d3
alameen738 <alameen738@gmail.com>
2023-06-28 12:31:13 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,13 +5,13 @@
  * @str: input
  * Return: print
  */
-void puts2(char *str) {
-    int i = 0;
-
-    while (str[i] != '\0') {
-        printf("%c", str[i]);
-        i += 2;
-    }
-
-    printf("\n");
+void puts2(char *str)
+{
+int i = 0;
+while (str[i] != '\0')
+{
+printf("%c", str[i]);
+i += 2;
+}
+printf("\n");
 }
