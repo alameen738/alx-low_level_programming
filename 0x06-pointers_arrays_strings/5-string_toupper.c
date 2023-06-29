1911319a6142c4603ac3d1b25bcb9b828d9fbfa8
alameen738 <alameen738@gmail.com>
2023-06-29 16:53:45 -0700

    ./5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - chage all lower to uppercase
+ * @n: pointer
+ * Return: n
+ */
+char *string_toupper(char *n)
+{
+int i;
+
+i = 0;
+while (n[i] != '\0')
+{
+if (n[i] >= '0' &&  n[i] <= 'z')
+n[i] = n[i] - 32;
+i++;
+}
+return (n);
+}
