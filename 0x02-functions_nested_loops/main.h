e107dd7bd4ec9dc203ab1267a614f91bf10eb155
alameen738 <alameen738@gmail.com>
2023-07-31 07:00:54 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,16 @@
 #ifdef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
 int _putchar(char c);
-void print+_alphabet(void);
-void print_alphabet_x10(viod);
+void print_alphabet(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
-int _print_sign(int n);
+int print_sign(int n);
 int _abs(int);
-int print_last_digigit(int);
-void jack,bauer(void);
+int print_last_digit(int);
+void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to98(int n);
-void print_times_table(int n);
+void print_to_98(int n);
 
 #endif
