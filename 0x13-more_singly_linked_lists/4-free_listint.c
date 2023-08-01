2a8a68e42254a52973a60383db5ff1cb78cd6910
alameen738 <alameen738@gmail.com>
2023-08-01 01:35:30 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+
+/**
+ * free_listint - free_listint
+ * @head: listint_t list to freed.
+ */
+
+void free_listint(listint_t *head)
+
+{
+listint_t *temp;
+while (head)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
+
+}
