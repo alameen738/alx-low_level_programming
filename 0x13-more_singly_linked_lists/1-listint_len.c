3027612af214d79c6e782ee87534a15f1068438e
alameen738 <alameen738@gmail.com>
2023-08-01 00:22:29 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+
+/**
+ * listint_len - Return  Number of elements linked lists
+ * @h: linked list of a type listint_t to traverse
+ *
+ * Return: Num of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+
+{
+
+size_t num = 0;
+while (h)
+{
+num++;
+h = h->next;
+
+}
+
+
+return (num);
+
+}
