fd887ace8ec182766704b515c425415138b29a7a
alameen738 <alameen738@gmail.com>
2023-08-03 12:40:21 -0700

    4-clear_bit.c

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ * clear_bit - clear_bit
+ * @n: pointer to the number to change
+ * @index: index of the bit to clear
+ *
+ * Return: Return 1 if (success), -1 if (failure)
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+
+{
+if (index > 63)
+return (-1);
+*n = (~(1UL << index) & *n);
+return (1);
+
+}
--- 0x14-bit_manipulation/4-main.c
+++ 0x14-bit_manipulation/4-main.c
@@ -1,4 +1,3 @@
-julien@ubuntu:~/0x14. Binary$ cat 4-main.c
 #include <stdio.h>
 #include "main.h"
 
