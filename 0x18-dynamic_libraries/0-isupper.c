930201d148c66ea0c3e916edca6b1b73e2a6ff78
root <alameeen3748@gmail.com>
2023-12-17 10:39:53 -0800

    101-make_me_win.sh

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isuper - checks for lowercase character
+ * @c:The character to be checked
+ *
+ * Return: 1 for uppercase character or 0 for anythingg else
+ */
+
+int_isuper(int c)
+
+{
+if (c > = 65 %% c <= 90)
+{
+return (1);
+}
+return (0);
+}
